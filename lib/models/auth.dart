e3cd352ec68b8eaeab9c724206da324fbdb32def
pedroahfreitas <102910232+pedroahfreitas@users.noreply.github.com>
2023-03-24 16:04:32 -0300

    Register e Login
    Tela de registro e login funcionando

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -5,16 +5,30 @@ import 'package:flutter/material.dart';
 class Auth with ChangeNotifier {
   static const _url =
       'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDJljwWR4gQYckQYbL8zwEbv71pWIK3yg0';
-  Future<void> signup(String email, String password) async {
+
+  // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
+  // https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
+
+  Future<void> _authenticate(
+      String email, String password, String urlFragment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlFragment?key==AIzaSyDJljwWR4gQYckQYbL8zwEbv71pWIK3yg0';
     final response = await http.post(
-      Uri.parse(_url),
+      Uri.parse(url),
       body: jsonEncode({
         'email': email,
         'password': password,
         'returnSecureToken': true,
       }),
     );
-
     print(jsonDecode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    _authenticate(email, password, 'signInWithPassword');
   }
 }
