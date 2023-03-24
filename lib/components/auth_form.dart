9a5a908c7c9d66a69b9d89376c56151931bbe0ab
pedroahfreitas <102910232+pedroahfreitas@users.noreply.github.com>
2023-03-24 15:20:18 -0300

    Alternar entre telas
    Tela de Login e tela de registro

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -11,13 +11,45 @@ class AuthForm extends StatefulWidget {
 
 class _AuthFormState extends State<AuthForm> {
   final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  bool _isLoading = false;
   AuthMode _authMode = AuthMode.Login;
   Map<String, String> _authData = {
     'email': '',
     'senha': '',
   };
 
-  void _submit() {}
+  bool _isLogin() => _authMode == AuthMode.Login;
+  bool _isSingup() => _authMode == AuthMode.Singup;
+
+  void _switchAuthMode() {
+    setState(() {
+      if (_isLogin()) {
+        _authMode = AuthMode.Singup;
+      } else {
+        _authMode = AuthMode.Login;
+      }
+    });
+  }
+
+  void _submit() {
+    final isValid = _formKey.currentState?.validate() ?? false;
+
+    if (!isValid) {
+      return;
+    }
+
+    setState(() => _isLoading = true);
+    _formKey.currentState?.save();
+
+    if (_isLogin()) {
+      //Login
+    } else {
+      //Registrar
+    }
+
+    setState(() => _isLoading = false);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,70 +61,81 @@ class _AuthFormState extends State<AuthForm> {
       ),
       child: Container(
         padding: const EdgeInsets.all(16),
-        height: 320,
+        height: _isLogin() ? 310 : 400,
         width: deviceSize.width * 0.75,
         child: Form(
-            child: Column(
-          children: [
-            TextFormField(
-              decoration: InputDecoration(labelText: 'E-mail'),
-              keyboardType: TextInputType.emailAddress,
-              onSaved: (email) => _authData['email'] = email ?? '',
-              validator: (_email) {
-                final email = _email ?? '';
-                if (email.trim().isEmpty || !email.contains('@')) {
-                  return 'Informe um e-mail válido';
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Senha'),
-              keyboardType: TextInputType.emailAddress,
-              obscureText: true,
-              controller: _passwordController,
-              onSaved: (password) => _authData['password'] = password ?? '',
-              validator: (_password) {
-                final password = _password ?? '';
-                if (password.isEmpty || password.length < 6) {
-                  return 'Infomr uma senha com no mínimo 6 caracteres.';
-                }
-                return null;
-              },
-            ),
-            if (_authMode == AuthMode.Singup)
+          key: _formKey,
+          child: Column(
+            children: [
               TextFormField(
-                decoration: InputDecoration(labelText: ' Confirmar Senha'),
+                decoration: InputDecoration(labelText: 'E-mail'),
+                keyboardType: TextInputType.emailAddress,
+                onSaved: (email) => _authData['email'] = email ?? '',
+                validator: (_email) {
+                  final email = _email ?? '';
+                  if (email.trim().isEmpty || !email.contains('@')) {
+                    return 'Informe um e-mail válido';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Senha'),
                 keyboardType: TextInputType.emailAddress,
                 obscureText: true,
-                validator: _authMode == AuthMode.Login
-                    ? null
-                    : (_password) {
-                        final password = _password ?? '';
-                        if (password != _passwordController.text) {
-                          return 'Senhas informadas não conferem.';
-                        } else {
-                          return null;
-                        }
-                      },
+                controller: _passwordController,
+                onSaved: (password) => _authData['password'] = password ?? '',
+                validator: (_password) {
+                  final password = _password ?? '';
+                  if (password.isEmpty || password.length < 6) {
+                    return 'Infomr uma senha com no mínimo 6 caracteres.';
+                  }
+                  return null;
+                },
               ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _submit,
-              child: Text(
-                _authMode == AuthMode.Login ? ' ENTRAR' : 'REGISTRAR',
-              ),
-              style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
+              if (_isSingup())
+                if (_isLoading)
+                  CircularProgressIndicator()
+                else
+                  TextFormField(
+                    decoration: InputDecoration(labelText: ' Confirmar Senha'),
+                    keyboardType: TextInputType.emailAddress,
+                    obscureText: true,
+                    validator: _isLogin()
+                        ? null
+                        : (_password) {
+                            final password = _password ?? '';
+                            if (password != _passwordController.text) {
+                              return 'Senhas informadas não conferem.';
+                            } else {
+                              return null;
+                            }
+                          },
                   ),
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 30,
-                    vertical: 8,
-                  )),
-            ),
-          ],
-        )),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _submit,
+                child: Text(
+                  _authMode == AuthMode.Login ? ' ENTRAR' : 'REGISTRAR',
+                ),
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 30,
+                      vertical: 8,
+                    )),
+              ),
+              Spacer(),
+              TextButton(
+                onPressed: _switchAuthMode,
+                child:
+                    Text(_isLogin() ? 'DESEJA REGISTRAR?' : 'JÁ POSSUI CONTA?'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
