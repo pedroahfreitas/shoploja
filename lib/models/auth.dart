546be255755b60fa69161973fc61e0a8d06f7032
pedroahfreitas <102910232+pedroahfreitas@users.noreply.github.com>
2023-03-25 15:12:43 -0300

    Final registro
    Finalizada parte de register

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -5,11 +5,17 @@ import 'package:http/http.dart' as http;
 
 class Auth with ChangeNotifier {
   static const _url =
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDJljwWR4gQYckQYbL8zwEbv71pWIK3yg0';
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key';
 
-  Future<void> signup(String email, String password) async {
+  // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
+  // https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
+
+  Future<void> _authenticate(
+      String email, String password, String urlFragment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlFragment?key=AIzaSyDJljwWR4gQYckQYbL8zwEbv71pWIK3yg0';
     final response = await http.post(
-      Uri.parse(_url),
+      Uri.parse(url),
       body: jsonEncode({
         'email': email,
         'password': password,
@@ -17,5 +23,13 @@ class Auth with ChangeNotifier {
       }),
     );
     print(jsonDecode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    _authenticate(email, password, 'signInWithPassword?');
   }
 }
